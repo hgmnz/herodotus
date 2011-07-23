b85586ed3b80b143e6db076b962a64448b5d1d11
Harold Giménez <hgimenez@thoughtbot.com>
2011-07-23 12:06:04 -0400

    Starts of the git interface

---
+++ lib/herodotus/git.rb
@@ -0,0 +1,27 @@
+module Herodotus
+  class Git
+    attr_accessor :repo
+    def initialize(base_dir)
+      @base_dir = base_dir
+      @repo     = ::Grit::Repo.new(guess_repo)
+    end
+
+    def commits
+      since = 'HEAD'
+      ::Grit::Commit.find_all(@repo, since)
+    end
+
+    private
+    def guess_repo
+      current_dir = File.expand_path(@base_dir)
+      until current_dir == '/' do
+        maybe_repo = File.expand_path(".git", current_dir)
+        if File.directory?(maybe_repo)
+          return maybe_repo
+        else
+          current_dir = File.expand_path('..', current_dir)
+        end
+      end
+    end
+  end
+end
---
+++ spec/git_spec.rb
@@ -0,0 +1,24 @@
+require File.expand_path('spec_helper', File.dirname(__FILE__))
+require 'ruby-debug'
+
+describe Herodotus::Git do
+  it 'locates the closest git repo' do
+    # TODO: FakeFS. Don't have the gem, and don't have internet, so moving forward with ugly mocks
+    # Can't stub a method on existing objects with minitest mocks
+    def File.expand_path(dir, root=nil); root ? "/#{root}/#{dir}" : dir; end
+    def File.directory?(dir); true; end
+    grit_repo = MiniTest::Mock.new
+    ::Grit::Repo.class_eval do
+      define_method :initialize do |path|
+        grit_repo.expect(:path, path)
+        @grit_repo = grit_repo
+        return @grit_repo
+      end
+      define_method :path do
+        @grit_repo.path
+      end
+    end
+    git = Herodotus::Git.new('bacon')
+    git.repo.path.must_equal '/bacon/.git'
+  end
+end
