20a6e62e3d112077c4390ff0c206d01b9e238efd
Harold Giménez <hgimenez@thoughtbot.com>
2011-07-29 18:54:59 -0400

    Only do some commits in tests when I need them

--- spec/collector_spec.rb
+++ spec/collector_spec.rb
@@ -4,34 +4,11 @@ describe Herodotus::Collector do
   def collector
     @collector ||= Herodotus::Collector.new('/tmp/herodotus')
   end
-  before do
-    FileUtils.mkdir_p '/tmp/herodotus'
-    FileUtils.cd '/tmp/herodotus' do
-      `git init`
-      `touch file1`
-      `git add file1`
-      `git commit -m "this is a change"`
-      `touch file2`
-      `git add file2`
-      `git commit -m "Another\nChAnGeLOg:\nBroke everything again. Don't update to this version."`
-      `touch file3`
-      `git add file3`
-      `git commit -m "Another\nchangelog:\nNevermind, everything is fixed now."`
-    end
-  end
-
-  after { FileUtils.rm_rf '/tmp/herodotus' }
 
   it 'starts off with a default git, changes and a since_ref of nil' do
     collector.git.wont_be_nil
     collector.since_ref.must_be_nil
     collector.changes.wont_be_nil
-  end
-
-  it 'finds commits containing the changelog keyword on the message' do
-    collector.changes.length.must_equal 2
-    collector.changes.first.message.must_equal "Broke everything again. Don't update to this version."
-    collector.changes.last.message.must_equal "Nevermind, everything is fixed now."
   end
 
   it 'tells the reporter to print' do
@@ -47,4 +24,31 @@ describe Herodotus::Collector do
     reporter.expects(:append_to_changelog).once
     collector.append_to_changelog
   end
+
+  describe 'with some commits' do
+    before do
+      FileUtils.mkdir_p '/tmp/herodotus'
+      FileUtils.cd '/tmp/herodotus' do
+        `git init`
+        `touch file1`
+        `git add file1`
+        `git commit -m "this is a change"`
+        `touch file2`
+        `git add file2`
+        `git commit -m "Another\nChAnGeLOg:\nBroke everything again. Don't update to this version."`
+        `touch file3`
+        `git add file3`
+        `git commit -m "Another\nchangelog:\nNevermind, everything is fixed now."`
+      end
+    end
+
+    after { FileUtils.rm_rf '/tmp/herodotus' }
+
+    it 'finds commits containing the changelog keyword on the message' do
+      collector.changes.length.must_equal 2
+      collector.changes.first.message.must_equal "Broke everything again. Don't update to this version."
+      collector.changes.last.message.must_equal "Nevermind, everything is fixed now."
+    end
+  end
+
 end
