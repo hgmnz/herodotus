7c84272f77455a53e48f6347ce1745b2c34832a4
Harold Giménez <hgimenez@thoughtbot.com>
2011-07-29 19:10:01 -0400

    Make rake tasks use the configured base path

--- lib/herodotus/tasks.rb
+++ lib/herodotus/tasks.rb
@@ -1,22 +1,26 @@
 require 'rake'
 require 'herodotus'
 
-def default_base_dir
-  File.expand_path(File.dirname(__FILE__))
-end
-
 namespace :herodotus do
+  def default_base_path
+    File.expand_path(File.dirname(__FILE__))
+  end
+
+  def configured_base_path
+    Herodotus::Configuration.config.base_path
+  end
+
   desc 'Prints out the change log from git'
   task :print, :since_ref do |t, args|
-    base_dir  = ENV['BASE_DIR'] || default_base_dir
-    Herodotus::Collector.new(base_dir, args['since_ref']).
+    base_path  = ENV['base_path'] || configured_base_path || default_base_path
+    Herodotus::Collector.new(base_path, args['since_ref']).
       print
   end
 
   desc 'Appends changes to your changelog'
   task :append, :since_ref do |t, args|
-    base_dir  = ENV['BASE_DIR'] || default_base_dir
-    Herodotus::Collector.new(base_dir, args['since_ref']).
+    base_path  = ENV['base_path'] || configured_base_path || default_base_path
+    Herodotus::Collector.new(base_path, args['since_ref']).
       append_to_changelog
   end
 end
